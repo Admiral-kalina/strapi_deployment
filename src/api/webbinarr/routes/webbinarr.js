819cc0798824ab24063f4beb3d7f712f0ffc8ff3
Kalinichenko <kalinamono@gmail.com>
2023-10-20 01:50:50 +0300

    delete webbinar collection

--- src/api/webbinarr/routes/webbinarr.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * webbinarr router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::webbinarr.webbinarr');
--- src/api/webbinarr/services/webbinarr.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * webbinarr service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::webbinarr.webbinarr');
